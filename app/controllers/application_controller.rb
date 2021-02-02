83d265946e48393f799da5c997df49f276c11b8b
Noah Adams <nhadams10@uchicago.edu>
2021-02-02 21:59:42 +0000

    partially done with payment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,45 @@ class ApplicationController < ActionController::Base
 
     render({ :template => "calculation_templates/square_results.html.erb"})
   end
+  
+  def blank_root_form
+
+    render({ :template => "calculation_templates/root_form.html.erb"})
+  end
+
+  def calculate_root
+
+    @num2 = params.fetch("user_number").to_f
+    @root_of_num2 = @num2 ** (0.5)
+
+    render({ :template => "calculation_templates/root_results.html.erb"})
+  end
+
+  def blank_payment_form
+
+    render({ :template => "calculation_templates/payment_form.html.erb"})
+  end
+
+  def calculate_payment
+
+    @num2 = params.fetch("user_number").to_f
+    @root_of_num2 = @num2 ** (0.5)
+
+    render({ :template => "calculation_templates/payment_results.html.erb"})
+  end
+
+  def blank_random_form
+
+    render({ :template => "calculation_templates/random_form.html.erb"})
+  end
+
+  def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/random_results.html.erb"})
+  end
 
 end
